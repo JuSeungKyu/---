994824c57c6e92653dba6c4960e7345922a9165b
JuSeungKyu <juseungkyu0711@gmail.com>
2021-03-15 00:07:23 +0900

    조금 남음

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-function asdf(inputArray){
-	sr = new File.OpenText();
-	while(true){
-		let outputArray = [];
-		for(let i = 0; i < 5; i+=1){
-			input = file.ReadLine();
-			if (input == null){
-                     break; 
-            }
-			outputArray.push(input);
+let questionList = [];
+let rightAnswer = [];
+let wrongAnswer = [[]];
+
+questionList.push("현재 수행 중에 있는 명령어 코드를 저장하고 있는 임시 저장 장치는?");
+rightAnswer.push("명령 레지스터");
+wrongAnswer.push(new Array("인덱스 레지스터", "누산기", "메모리 레지스터"));
+
+function randomNum(min, max){
+	let randNum = Math.floor(Math.random()*(max-min+1)) + min;
+	return randNum;
+}
+
+function writeQuestion(randomNumber){
+	rightAnswerNumber = randomNum(0, 4);
+	answer = rightAnswerNumber;
+
+	console.log(rightAnswerNumber);
+	questionText.innerHTML = questionList[randomNumber];
+	textArray[0].innerHTML = rightAnswer[randomNumber];
+
+	for(let i=0;i<4;i+=1){
+		if(i < rightAnswerNumber){
+			textArray[i].innerHTML = wrongAnswer[randomNumber+1][i];
+			console.log(textArray[i]);
 		}
-		inputArray.push(outputArray);
+		else if(i == rightAnswerNumber){
+			textArray[i].innerHTML = rightAnswer[randomNumber];
+			console.log(textArray[i]);
+		}
+		else{
+			textArray[i].innerHTML = wrongAnswer[randomNumber+1][i-1];
+			console.log(textArray[i]);
+		}
 	}
-	return inputArray
+
+	for(let i=1;i<4;i+=1){
+		textArray[i].innerHTML = wrongAnswer[randomNumber+1][i-1];
+	}
 }
 
 function choice(input){
@@ -24,17 +49,20 @@ function choice(input){
 	}
 }
 
-let arr = [];
-arr = asdf(arr)
-
 answer = 3;
 let choiceNumber = 5;
 let textArray = [];
 
-for(let i = 0; i < 5; i+=1){
+for(let i = 0; i < 4; i+=1){
 	str = "#text" + i;
 	textArray.push(document.querySelector(str));
 }
+
+
+questionText = document.querySelector("#question");
+
+let ranint = Math.floor(Math.random() * questionList.length);
+writeQuestion(0);
 
 document.querySelector("#choice0").onclick = function() {
 	choice(0);
@@ -47,8 +75,5 @@ document.querySelector("#choice2").onclick = function() {
 }
 document.querySelector("#choice3").onclick = function() {
 	choice(3);
-}
-document.querySelector("#choice4").onclick = function() {
-	choice(4);
 }
 
